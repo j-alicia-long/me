bd14fcd24335b57a1803e7d2139d9bd7d063537f
Jenny246844 <Mayflower842@gmail.com>
2017-11-27 00:56:06 -0500

    Initial revamp

--- README.md
+++
@@ -1,2 +0,0 @@
-# j-alicia-long.github.io
-My Portfolio Website
---
+++ js/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing');
+    });
+    
+});
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
